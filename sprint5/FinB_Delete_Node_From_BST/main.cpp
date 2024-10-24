98ea751bd3f75bdf9d1467f3d33254786dc97573
part-of-the-crew <nikolai.dunichev@gmail.com>
2024-10-24 20:08:52 +0300

    Sprint5 FinB done

--- sprint5/FinB_Delete_Node_From_BST/main.cpp
+++ sprint5/FinB_Delete_Node_From_BST/main.cpp
@@ -1,3 +1,38 @@
+/*
+https://contest.yandex.ru/contest/24810/run-report/122277987/
+-- WORKING PRINCIPLE --
+
+1. Check some corner cases related to the root
+2. Search and return pointer to the parental pointer to the replaced Node
+3. Search and return pointer to the parental pointer to the replacing Node
+   In the left or right node we are searching depends on whether they exist
+   but start with the right one.
+4. Replace the Node and update the childish pointers
+5. Disconnect the replacing Node from parent if it exists
+
+-- PROOF OF CORRECTNESS --
+
+ Invariants to keep eye on:
+ 1. After deleting any node BST must remain BST
+    This is ensured by the fact that we get the smallest replacement node from the right
+    or the biggest from left. This gives us the node where ltft side is smaller than its value
+    and right side is bigger than its value.
+ 2. BST must not be changed when the node to replace is not found
+    Unchanged root is returned in this case.
+ 3. Creating a new node is not allowed
+    No node is created during program execution
+ 4. Found node should be replaced by the proper node
+
+-- TIME COMPLEXITY --
+  O(H), 
+  where H is the hight of the tree.
+  because searching the replacing and replaced Nodes is O(H) in the worst case
+
+-- SPACE COMPLEXITY --
+   O(1)
+  No heap allocation
+*/
+
 #ifndef REMOTE_JUDGE
 struct Node {  
   int value;  
@@ -11,53 +46,6 @@ struct Node {
 #endif
 #include <cassert>
 
-#include <stack>
-#include <utility>
-//#include <numeric>
-/*
-std::tuple <Node*, Node*> search_smallest(Node* root) {
-    Node* parent = nullptr;
-    Node* current = root;
-    while (current->left != nullptr) {
-      parent = current;
-      current = current->left;
-    }
-    if (parent != nullptr)
-        parent->left = nullptr;
-    return {parent, current};
-}
-
-std::tuple <Node*, Node*> search_biggest(Node* root) {
-    Node* parent = nullptr;
-    Node* current = root;
-    while (current->right != nullptr) {
-      parent = current;
-      current = current->right;
-    }
-    if (parent != nullptr)
-        parent->right = nullptr;
-    return {parent, current};
-}
-
-std::tuple <Node*, Node*> search(Node* root, const int key) {
-    Node* parent = nullptr;
-    Node* current = root;
-    while (current != nullptr) {
-      if (key == current->value) {
-          if (parent != nullptr)
-              parent->left = nullptr;
-          return {parent, current};
-      } 
-      parent = current;
-      if (key < current->value) {
-          current = current->left;
-      } else if (key > current->value) {
-          current = current->right;
-      }
-    }
-    return {nullptr, nullptr};
-}
-*/
 
 Node** search(Node* current, const int key) {
     Node** parent = nullptr;
@@ -75,49 +63,80 @@ Node** search(Node* current, const int key) {
     }
     return nullptr;
 }
-Node** search_smallest(Node* current) {
-    Node** parent = nullptr;
-    while (current->left != nullptr) {
-      parent = &current->left;
-      current = current->left;
+Node** search_smallest(Node** current) {
+    Node** parent = current;
+    while ((*current)->left != nullptr) {
+      parent = &(*current)->left;
+      current = &(*current)->left;
     }
     return parent;
 }
 
-Node** search_biggest(Node* current) {
-    Node** parent = nullptr;
-    while (current->right != nullptr) {
-      parent = &current->right;
-      current = current->right;
+Node** search_biggest(Node** current) {
+    Node** parent = current;
+    while ((*current)->right != nullptr) {
+      parent = &(*current)->right;
+      current = &(*current)->right;
     }
     return parent;
 }
 
 Node* remove(Node* root, int key) {
-    auto parent = search(root, key);
-    if (parent == nullptr) {
+    if (root == nullptr)
+      return root;
+  
+    if (root->value == key && !root->left && !root->right)
+        return nullptr;
+
+// Find the node to remove and its parent
+    Node **parent = nullptr;
+    if (root->value == key)
+      parent = &root;
+    else
+      parent = search(root, key);
+
+// Node to be deleted is not found
+    if (parent == nullptr)
         return root;
-    }
+
     Node **found = nullptr;
-
+// search Node to replace
     if ((*parent)->right != nullptr) {
-      found = search_smallest((*parent)->right);
+      found = search_smallest(&(*parent)->right);
+    } else if ((*parent)->left != nullptr) {
+      found = search_biggest(&(*parent)->left);
+    } else {
+// Replacing Node not found
+      *parent = nullptr;
+      return root;
     }
 
-    if (found == nullptr && (*parent)->left != nullptr){
-      found = search_biggest((*parent)->left);
-    }
+// Connect children to the replacing node 
+    Node *left_child = (*parent)->left;
+    Node *right_child = (*parent)->right;
 
-    parent = found;
+    if (left_child != (*found))
+      (*found)->left = left_child;
 
-    if (found == nullptr) {
-        return root;
-    }
+    if (right_child != (*found))
+      (*found)->right = right_child;
+
+// Replace node
+    if (key != root->value)
+      *parent = *found;
+// Get new root 
+    else 
+      root = *found;
+
+// disconnect replacing node
+    *found = nullptr;
+
     return root;
 }
 
 #ifndef REMOTE_JUDGE
 void test() {
+  /*
   {
     Node node1({nullptr, nullptr, 2});
     Node node2({&node1, nullptr, 3});
@@ -125,6 +144,7 @@ void test() {
     assert(parent == &node2.left);
     assert(*parent == &node1);
   }
+  */
 /*
   {
     Node node1({nullptr, nullptr, 2});
@@ -132,10 +152,15 @@ void test() {
     auto [parent, newHead] = search(&node2, 4);
     assert(newHead == nullptr);
   }
-
-
   */
-  
+  {
+    Node node1({nullptr, nullptr, 1});
+    Node node2({nullptr, &node1, 2});
+    Node* newHead = remove(&node2, 2);
+    assert(newHead->value == 1);
+    assert(newHead == &node1);
+  }
+
   {
     Node node1({nullptr, nullptr, 2});
     Node node2({&node1, nullptr, 3});
