40a8c3143a228f0f4f1fece9d948c42f4e207a01
part-of-the-crew <nikolai.dunichev@gmail.com>
2024-11-16 22:35:43 +0200

    Sprint6 dij fix

--- sprint6/K_Dijkstra/main.cpp
+++ sprint6/K_Dijkstra/main.cpp
@@ -32,121 +32,103 @@ void print(const C& s) {
     std::cout << std::endl;
 }
 
-void relax(int u, int v) {
+
+struct Graph {
+    Graph(int N) : vertices(N), matrix(N, std::vector<int>(N, 0)) {}
+    int vertices;
+    //int edge;
+    std::vector<std::pair<int, int>> outgoingEdges(int v) {
+        auto it = edges.find(v);
+        if (it != edges.end())
+            return it->second;
+        return std::vector<std::pair<int, int>> {};
+    }
+    //private:
+    std::map<int, std::vector<std::pair<int, int>>> edges;
+    std::vector<std::vector<int>> matrix;
+    //std::vector<std::vector<int>> matrix(N, std::vector<int>(N, 0));
+};
+
+struct GraphDetails {
+    GraphDetails(const Graph &graph) :
+                        dist(graph.vertices + 1, std::numeric_limits<int>::max()),
+                        previous(graph.vertices + 1, -1),
+                        visited(graph.vertices + 1, false) {};
+    std::vector<int> dist;
+    std::vector<int> previous;          // SPT.
+    std::vector<bool> visited;
+};
+
+int weight(Graph &graph, int u, int v)
+{
+    auto vec = graph.edges[u];
+    for (auto &e : vec) {
+        if (e.first == v) {
+            return e.second;
+        }
+    }
+    return -1;
+}
+
+void relax(Graph &graph, GraphDetails &gd, int u, int v) {
     // Проверяем, не получился ли путь короче найденного ранее.
-    if (dist[v] > dist[u] + weight(u, v)) {
-        dist[v] = dist[u] + weight(u, v);
-        previous[v] = u;
+    //u - current vertix
+    if (gd.dist[v] > gd.dist[u] + weight(graph, u, v)) {
+        gd.dist[v] = gd.dist[u] + weight(graph, u, v);
+        gd.previous[v] = u;
     }
 }
 
-int get_min_dist_not_visited_vertex() {
+int get_min_dist_not_visited_vertex(GraphDetails &gd, Graph &gr) {
     // Находим ещё непосещённую вершину с минимальным расстоянием от s.
-    int currentMinimum = INT_MAX;
+    int currentMinimum = std::numeric_limits<int>::max();
     int currentMinimumVertex = -1;
 
-    for (int v : graph.vertices) {
-        if (!visited[v] && dist[v] < currentMinimum) {
-            currentMinimum = dist[v];
+    for (int v = 1; v <= gr.vertices; v++) {
+        if (!gd.visited[v] && gd.dist[v] < currentMinimum) {
+            currentMinimum = gd.dist[v];
             currentMinimumVertex = v;
         }
     }
     return currentMinimumVertex;
 }
 
-void dijkstra(Graph graph, int s) {
-    for (int v : graph.vertices) {
-        dist[v] = std::numeric_limits<int>::max();         // Задаём расстояние по умолчанию.
-        previous[v] = -1;          // Задаём предшественника для восстановления SPT.
-        visited[v] = false;        // Список статусов посещённости вершин.
-    }
+void dijkstra(int s, Graph &graph ) {
+    GraphDetails gd(graph);
 
-    dist[s] = 0;     // Расстояние от вершины до самой себя 0.
+    gd.dist[s] = 0;     // Расстояние от вершины до самой себя 0.
 
     while (true) {
-        int u = get_min_dist_not_visited_vertex();
+        int u = get_min_dist_not_visited_vertex(gd, graph);
 
-        if (u == -1 || dist[u] == INT_MAX) {
+        if (u == -1 || gd.dist[u] == std::numeric_limits<int>::max()) {
             break;
         }
 
-        visited[u] = true;
+        gd.visited[u] = true;
         // из множества рёбер графа выбираем те, которые исходят из вершины u
         std::vector<std::pair<int, int>> neighbours = graph.outgoingEdges(u);
 
-        for (auto [_, v] : neighbours) {
-            relax(u, v);
+        for (auto [v, _] : neighbours) {
+            relax(graph, gd, u, v);
         }
     }
 } 
 
-void DFS(int start_vertex, const std::map <int, std::vector<std::pair<int,int>>> &gr,
-             std::vector <int> &color, std::vector<std::vector<int>> traversal)
+
+
+void Main(Graph &gr)
 {
-
-    color[start_vertex] = 1;
-    std::queue<int> planned;
-    planned.push(start_vertex);
-    traversal.push_back(start_vertex);
-    while (!planned.empty()) {
-
-        int v = planned.front();
-        planned.pop();
-        
-        if (auto it = gr.find(v); it != gr.end())
-          for (int w : it->second) {
-              if (color[w] == -1) {
-                  color[w] = 1;
-                  planned.push(w);
-                  traversal.push_back(w);
-              }
-          }
-        color[v] = 2;
+    //std::vector <int> colorst(gr.vertices + 1, -1);   
+    for (int i = 1; i <= gr.vertices; ++i) {
+      dijkstra(i, gr);
     }
-}
 /*
-    std::map <int, std::vector<int>> gr = {
-        {1, {2, 3}},
-        {2, {4}},
-        {3, {5}},
-        {4, {}},
-        {5, {}}
-}
-*/
-struct Graph {
-    Graph(int N, int M) : vertices(N), edge(M), matrix(N, std::vector<int>(N, 0)) {}
-    int vertices;
-    int edge;
-    std::vector<std::pair<int, int>> outgoingEdges(int v) const {
-        return std::vector<std::pair<int, int>>{edges.at(v)};
-    }
-    //private:
-    std::map<int, std::vector<std::pair<int, int>>> edges;
-    std::vector<std::vector<int>> matrix;
-};
-struct GraphDetails {
-    GraphDetails(const Graph &graph) :
-                        dist(graph.vertices, std::numeric_limits<int>::max()),
-                        previous(graph.vertices, -1),
-                        visited(graph.vertices, false) {}
-    std::vector<int> dist;
-    std::vector<int> previous;          // SPT.
-    std::vector<bool> visited;
-};
-void MainDFS( int N, const std::map <int, std::vector<std::pair<int,int>>> &gr,
-             std::vector <int> &color)
-{
-    
-    std::vector<std::vector<int>> matrix(N, std::vector<int>(N, 0));
-
-    for (int i = 0; i < N; ++i) {
-      DFS(N, gr, color, matrix);
-    }
-
     for(const auto &v: matrix)
     {
       print(v);
     }
+*/
 }
 
 
@@ -159,8 +141,8 @@ int main () {
 	int N, M;
 	std::cin >> N >> M;
     std::cin.ignore();
-	std::map <int, std::vector<std::pair<int,int>>> gr;
-    std::vector <int> colorst(N + 1, -1);
+	//std::map <int, std::vector<std::pair<int,int>>> gr;
+    Graph gr(N);
 
 	for (int i = 0; i < M; ++i)
     {
@@ -168,19 +150,19 @@ int main () {
         int c;
         std::cin >> a >> b;
         std::cin >> c;
-        gr[a].push_back({b, c});
-        gr[b].push_back({a, c}); //uncomment for non-oriented graph
+        gr.edges[a].push_back({b, c});
+        gr.edges[b].push_back({a, c}); //uncomment for non-oriented graph
         std::cin.ignore();
     }
 
-  for (auto &e: gr)
+  for (auto &e: gr.edges)
   {
     std::sort(e.second.begin(), e.second.end());
   }
 
   //int start = std::chrono::high_resolution_clock::now();
 
-  MainDFS(N, gr, colorst);
+  Main(gr);
 
   //std::cout << std::endl;
 
